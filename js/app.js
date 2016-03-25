5d3807eddb6531fd634f995b734045c72f083d6c
Jase <jase@star29.com>
2016-03-25 15:16:59 -0600

    tuning working carousel with colors

--- js/app.js
+++ js/app.js
@@ -7,15 +7,29 @@
     $scope.active = -1;
     $scope.tweets = [];
 
+    var intervalId = false;
+    var startInterval = function(){
+      return $interval(function(idx){
+        if($scope.tweets.length) {
+          $scope.active = (($scope.active + 1) % ($scope.tweets.length));
+        }
+        if(idx % $scope.tweets.length === 0) {
+          init();
+        }
+      }, 1000);
+    };
+
     var init = function(){
       tweets.getTweets().then(
         function success(response){
+          if(!intervalId) {
+            intervalId = startInterval();
+          }
           var data = response.data;
           window.console.log(data.statuses[0]);
           $scope.tweets = data.statuses.reverse();
           // $scope.tweets = _.first(data.statuses.reverse(), 5);
           $scope.search = data.search_metadata;
-          $scope.active = 0;
         },
         function fail(err){
           console.log('error', err);
@@ -26,23 +40,15 @@
     init();
 
     $scope.styleCard = function(tweet){
+      if(tweet.user.profile_background_color === tweet.user.profile_text_color) {
+        tweet.user.profile_text_color = 'FFFFFF';
+      }
       return {
         backgroundColor: '#' + tweet.user.profile_background_color,
-        backgroundImage: 'url('+tweet.user.profile_background_image_url+')',
+        // backgroundImage: 'url('+tweet.user.profile_background_image_url+')',
         color: '#' + tweet.user.profile_text_color
       }
     };
-
-    $interval(function(idx){
-      if(idx % $scope.tweets.length === 0) {
-        window.console.log('hello', idx);
-        init();
-      } else {
-        if($scope.tweets.length) {
-          $scope.active = (($scope.active + 1) % ($scope.tweets.length));
-        }
-      }
-    }, 1500);
 
     $scope.$watch('active', function(n){
       var el = angular.element('.active');
