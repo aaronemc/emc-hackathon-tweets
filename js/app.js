fcfb0d24f3f8a9d56941714ee9261f2e991fe682
Jase <jase@star29.com>
2016-03-25 16:33:27 -0600

    color brightness for tweet text color detection

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 (function(angular, _, moment) {
   'use strict';
 
+  var brightness = function(color){
+    var r, g, b, brightness;
+
+    if (color.match(/^rgb/)) {
+      color = color.match(/rgb\(([^)]+)\)/)[1];
+      color = color.split(/ *, */).map(Number);
+      r = color[0];
+      g = color[1];
+      b = color[2];
+    } else if ('#' === color[0] && 7 === color.length) {
+      r = parseInt(color.slice(1, 3), 16);
+      g = parseInt(color.slice(3, 5), 16);
+      b = parseInt(color.slice(5, 7), 16);
+    } else if ('#' === color[0] && 4 === color.length) {
+      r = parseInt(color[1] + color[1], 16);
+      g = parseInt(color[2] + color[2], 16);
+      b = parseInt(color[3] + color[3], 16);
+    }
+
+    brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+    return brightness;
+  };
+
   angular.module('app', [])
   .controller('controller', ['$scope','$interval', 'tweetsService', function($scope, $interval, tweets) {
 
@@ -16,7 +40,7 @@
         if(idx % $scope.tweets.length === 0) {
           init();
         }
-      }, 1000);
+      }, 7500);
     };
 
     var init = function(){
@@ -40,13 +64,16 @@
     init();
 
     $scope.styleCard = function(tweet){
-      if(tweet.user.profile_background_color === tweet.user.profile_text_color) {
-        tweet.user.profile_text_color = 'FFFFFF';
-      }
+      var bg = '#' + tweet.user.profile_background_color;
+
+      var bgBrightness = brightness(bg);
+
+      var textColor = bgBrightness < 155 ? '#FFFFFF' : '#000000';
+
       return {
         backgroundColor: '#' + tweet.user.profile_background_color,
         // backgroundImage: 'url('+tweet.user.profile_background_image_url+')',
-        color: '#' + tweet.user.profile_text_color
+        color: textColor
       }
     };
 
