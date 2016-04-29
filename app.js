03419b4d2693aadc4988affb9235b881d759df77
Christian Parker <christian.parker@emc.com>
2016-04-29 14:32:31 -0600

    building minimal

---
+++ app.js
@@ -0,0 +1,8 @@
+(function(angular, _, moment) {
+  'use strict';
+
+  angular.module('emcTweetApp', [])
+    .controller('main-controller', ['$scope', function($scope) {
+    }])
+
+})(window.angular, window._, window.moment);
