91420d023a3165a719fc0921340eec71ea285be8
Kris Thompson <kristhompson@Kriss-MacBook-Pro.local>
2016-08-29 13:39:55 -0600

    adding continous running task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch'),
+    server = require('gulp-express');
 
 
 // Lint Task
@@ -19,8 +21,29 @@ gulp.task('minify', function () {
         .pipe(gulp.dest('build'))
 });
 
+gulp.task('stream', function () {
+    // Endless stream mode
+    return watch('sass/**/*.scss', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('server', function () {
+    // Start the server at the beginning of the task
+    server.run(['server.js']);
+
+    // Restart the server when file changes
+    gulp.watch(['sass/**/*.scss'], ['styles:scss']);
+
+
+    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+    //gulp.watch(['app/images/**/*'], server.notify);
+    gulp.watch(['server.js'], [server.run]);
+});
+
+
 gulp.task('default', function() {
     // place code for your default task here
 });
 
-gulp.task('default', ['lint']);
+gulp.task('default', ['server']);
